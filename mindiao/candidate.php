da08367cfa715afc3193a1274b8c1878739279bf
jianada-qianzheng <39447674+jianada-qianzheng@users.noreply.github.com>
2020-01-05 11:25:35 -0500

    Update candidate.php

--- mindiao/candidate.php
+++ mindiao/candidate.php
@@ -15,22 +15,22 @@
  // Check if there are results
  if ($result = mysqli_query($con, $sql))
  {
-  // If so, then create a results array and a temporary one
-  // to hold the data
-  $resultArray = array();
-  $tempArray = array();
+    // If so, then create a results array and a temporary one
+    // to hold the data
+    $resultArray = array();
+    $tempArray = array();
  
-  // Loop through each row in the result set
-  while($row = $result->fetch_object())
-  {
-  // Add each row into our results array
-   $tempArray = $row;
-   array_push($resultArray, $tempArray);
-  }
+    // Loop through each row in the result set
+    while($row = $result->fetch_object())
+    {
+        // Add each row into our results array
+        $tempArray = $row;
+        array_push($resultArray, $tempArray);
+    }
  
-  // Finally, encode the array to JSON and output the results
-  echo json_encode($resultArray);
-}
+    // Finally, encode the array to JSON and output the results
+    echo json_encode($resultArray);
+ }
  
  // Close connections
  mysqli_close($con);
